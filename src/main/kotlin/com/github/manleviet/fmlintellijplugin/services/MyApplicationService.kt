d55786ee58dc44fd0ade268a2550fd23aed3620a
GitHub Action <action@github.com>
2022-07-22 22:39:48 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/manleviet/fmlintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.manleviet.fmlintellijplugin.services
+
+import com.github.manleviet.fmlintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/manleviet/fmlintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/manleviet/fmlintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.manleviet.fmlintellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.manleviet.fmlintellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
